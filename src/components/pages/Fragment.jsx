95137543e44e8fac399529393a6202284e74aed3
Konstantin Tuhay <tugay-k@mail.ru>
2024-06-18 16:49:15 +0300

    add: content refs

--- src/components/pages/Fragment.jsx
+++ src/components/pages/Fragment.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import NavigateHelpers from "../../helpers/NavigateHelpers";
 
 const ReactThis = (props) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   // const general = () => {
   //   navigate("/");
@@ -16,6 +16,16 @@ const ReactThis = (props) => {
   return (
     <div>
       <h2>Fragment</h2>
+
+      <p>
+        С помощью фрагментов можно сгруппировать список дочерних элементов без
+        добавления дополнительных узлов в DOM.
+      </p>
+
+      <p>
+        Используем React.Fragment или выполняем импорт, чтобы использовать
+        Fragment:
+      </p>
 
       <NavigateHelpers callback={props.goToPage} txt="На глвыную" path="/" />
       <NavigateHelpers callback={props.goToPage} />
--- src/components/pages/Refs.jsx
+++ src/components/pages/Refs.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import NavigateHelpers from "../../helpers/NavigateHelpers";
 
 const ReactThis = (props) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   // const general = () => {
   //   navigate("/");
@@ -16,6 +16,66 @@ const ReactThis = (props) => {
   return (
     <div>
       <h2>Refs</h2>
+
+      <p>
+        Refs - предоставляет способ доступа к DOM-узлам или React-элементам,
+        созданным в методе render()
+      </p>
+
+      <p>Использование рефов:</p>
+
+      <ol>
+        <li>Управление фокусом, выделение текста.</li>
+        <li>Выполнение анимаций.</li>
+        <li>Интеграция со сторонними библиотеками, взаимодействующие с DOM.</li>
+      </ol>
+
+      <p>Пример с управлением фокусом</p>
+
+      <pre id="code">
+        <code>
+          {`
+          import React, { useEffect, useState, Fragment, useRef } from 'react'
+
+          const Test = () => {
+              const [text, setText] = useState('')
+              const textInput = useRef(null)
+              const valid = () => {
+                  textInput.current.focus()
+              }
+              return (
+                  <div>
+                      <input ref={textInput} value={text} />
+                      <button onClick={() => valid()}>
+                          Click to focus
+                      </button>
+                  </div>
+              )
+          }
+
+          export default Test
+          `}
+        </code>
+      </pre>
+
+      <p>Или при первом render, чтобы сразу был фокус:</p>
+
+      <pre id="code">
+        <code>
+          {`
+          const refFocus = useRef(null);
+
+          useEffect(() => {
+            refFocus.current.focus();
+          }, []);
+          
+          <input
+            placeholder="Введите имя"
+            ref={refFocus}
+          />
+          `}
+        </code>
+      </pre>
 
       <NavigateHelpers callback={props.goToPage} txt="На глвыную" path="/" />
       <NavigateHelpers callback={props.goToPage} />
