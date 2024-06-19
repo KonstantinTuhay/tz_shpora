a4ce2a6145c31caa00294a49108be1300f076ce6
Konstantin Tuhay <tugay-k@mail.ru>
2024-06-19 11:49:18 +0300

    add: content fragment

--- src/components/pages/Fragment.jsx
+++ src/components/pages/Fragment.jsx
@@ -27,6 +27,44 @@ const ReactThis = (props) => {
         Fragment:
       </p>
 
+      <pre id="code">
+        <code>
+          {`
+          import React, { Fragment } from 'react';
+
+          //Заменяем теги div на Fragment:
+          const ChildComponent = () => <Fragment>
+              <h1>Hello Child Component</h1>
+              <h1>Hello Child Component</h1>
+          </Fragment>
+          `}
+        </code>
+      </pre>
+
+      <p>
+        Существуют сокращенная запись. Недостаток сокращённой записи: нельзя
+        использовать атрибуты, например className или key:
+      </p>
+
+      <pre id="code">
+        <code>
+          {`
+          import React from "react";
+
+          const App = () => {
+            return (
+              <>
+                <p>Something ...</p>
+                <p>Something ...</p>
+              </>
+            );
+          };
+
+          export default App;
+          `}
+        </code>
+      </pre>
+
       <NavigateHelpers callback={props.goToPage} txt="На глвыную" path="/" />
       <NavigateHelpers callback={props.goToPage} />
     </div>
